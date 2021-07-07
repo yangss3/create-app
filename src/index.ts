6650b7daff7c2db7e0ffa5594ec69db8bbe0b1e5
yangss3 <yss_2016@outlook.com>
2021-07-07 23:34:43 +0800

    feat: selection of ui library on pc template

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,20 @@ const spinner = ora()
 const exec = promisify(execCb)
 const argv = minimist(process.argv.slice(2))
 const cwd = process.cwd()
-const TEMPLATES = ['web', 'pc', 'mobile', 'vanilla']
-const renameFiles: Record<string, string> = {
+const renameRootFiles: Record<string, string> = {
   _gitignore: '.gitignore'
 }
+const templates = ['web', 'pc', 'mobile', 'vanilla'] as const
+const pcUiLibs = [
+  { name: 'Ant Design Vue', value: 'antdv' }
+] as const
+const mobileUiLibs = [
+  { name: 'Vant', value: 'vant' }
+] as const
+
+type Template = typeof templates[number]
+type PcUiLib = typeof pcUiLibs[number]['value']
+type MobileUiLib = typeof mobileUiLibs[number]['value']
 
 async function init () {
   let targetDir = argv._[0]
@@ -29,9 +39,9 @@ async function init () {
     targetDir = name
   }
 
-  const dist = path.resolve(cwd, targetDir)
+  const dest = path.resolve(cwd, targetDir)
 
-  if (fs.existsSync(dist)) {
+  if (fs.existsSync(dest)) {
     const { yes } = await prompt({
       type: 'confirm',
       name: 'yes',
@@ -41,19 +51,19 @@ async function init () {
     })
     if (yes) {
       spinner.start('Removing...')
-      await fs.remove(dist)
+      await fs.remove(dest)
       spinner.succeed('Removed')
     } else {
       return
     }
   }
 
-  let template: string = argv.t || argv.template
+  let template: Template = argv.t || argv.template
   let message = 'Select a template:'
   let isValidTemplate = false
 
   if (typeof template === 'string') {
-    isValidTemplate = TEMPLATES.includes(template)
+    isValidTemplate = templates.includes(template)
     message = `${template} isn't a valid template. Please choose from below:`
   }
 
@@ -62,27 +72,31 @@ async function init () {
       type: 'list',
       name: 't',
       message,
-      choices: TEMPLATES
+      choices: templates
     })
     template = t
   }
 
-  const templateDir = path.resolve(__dirname, `../templates/${template}`)
-
-  const files = fs.readdirSync(templateDir)
-  for (const file of files) {
-    renameFiles[file]
-      ? fs.copySync(path.join(templateDir, file), path.join(dist, renameFiles[file]))
-      : fs.copySync(path.join(templateDir, file), path.join(dist, file))
+  let uiLib: PcUiLib | MobileUiLib | undefined
+  if (template === 'pc') {
+    const { ui } = await prompt({
+      type: 'list',
+      name: 'ui',
+      message,
+      choices: pcUiLibs
+    })
+    uiLib = ui
   }
 
+  generateTemplate(dest, template, uiLib)
+
   spinner.start('Install deps...')
-  const installOpt = await exec(`cd ${dist} && npm install`)
+  const installOpt = await exec(`cd ${dest} && npm install`)
   spinner.succeed('Install deps completed')
   console.log(installOpt.stdout)
 
   const initGitRepo = [
-    `cd ${dist}`,
+    `cd ${dest}`,
     'npm install @yangss/init-git-repo -D',
     'npx init-git-repo'
   ]
@@ -91,9 +105,9 @@ async function init () {
   spinner.succeed('Initialize git repo completed')
   console.log(initOpt.stdout)
 
-  const pkgJsonPath = path.join(dist, 'package.json')
+  const pkgJsonPath = path.join(dest, 'package.json')
   const pkgJson = fs.readJsonSync(pkgJsonPath)
-  pkgJson.name = path.basename(dist)
+  pkgJson.name = path.basename(dest)
   pkgJson['lint-staged'] = template === 'vanilla'
     ? { '*.(ts|js)': 'eslint --fix' }
     : { '*.(ts|tsx|vue)': 'eslint --fix' }
@@ -104,13 +118,61 @@ async function init () {
     console.log()
   } else {
     console.log('\nDone. Now run:\n')
-    if (dist !== cwd) {
-      console.log(` cd ${path.relative(cwd, dist)}`)
+    if (dest !== cwd) {
+      console.log(` cd ${path.relative(cwd, dest)}`)
     }
     console.log(' npm run dev')
     console.log()
   }
 }
+
+function generateTemplate<T extends Template> (
+  dest: string,
+  template: T,
+  uiLib: T extends 'pc'
+    ? PcUiLib
+    : T extends 'mobile'
+      ? MobileUiLib
+      : undefined
+) {
+  const templateDir = path.resolve(__dirname, `../templates/${template}`)
+
+  const files = fs.readdirSync(templateDir)
+  for (const file of files) {
+    const templateRootFile = path.join(templateDir, file)
+    renameRootFiles[file]
+      ? fs.copySync(templateRootFile, path.join(dest, renameRootFiles[file]))
+      : fs.copySync(templateRootFile, path.join(dest, file))
+  }
+
+  const renameSrcFiles = (dir: string, files: string[]) => {
+    const regex = /^.+(?<lib>\..+)\..+$/
+    for (const file of files) {
+      const oldFile = path.join(dir, file)
+      const matched = regex.exec(file)
+      if (matched) {
+        if (matched.groups?.lib?.slice(1) === uiLib) {
+          fs.renameSync(
+            oldFile,
+            path.join(dir, file.replace(matched.groups?.lib, ''))
+          )
+        } else {
+          fs.unlinkSync(oldFile)
+        }
+      }
+    }
+  }
+
+  if (template === 'pc') {
+    const destSrcPath = path.join(dest, 'src')
+    const destSrcViewPath = path.join(dest, 'src/views')
+    const destSrcFiles = fs.readdirSync(destSrcPath)
+    const destSrcViewFiles = fs.readdirSync(destSrcViewPath)
+    renameSrcFiles(destSrcPath, destSrcFiles)
+    renameSrcFiles(destSrcViewPath, destSrcViewFiles)
+  }
+}
+
 
 init().catch((e) => {
   console.error(e)
